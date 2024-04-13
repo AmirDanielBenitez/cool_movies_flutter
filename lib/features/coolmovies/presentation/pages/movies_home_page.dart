5ee21501eabe980a173f08391db3a7c7522b0813
Amir Benitez <amir.daniel.b.d@gmail.com>
2024-04-13 14:49:07 -0400

    Initial movie list page design

--- lib/features/coolmovies/presentation/pages/movies_home_page.dart
+++ lib/features/coolmovies/presentation/pages/movies_home_page.dart
@@ -1,6 +1,9 @@
+import 'package:coolmovies/features/coolmovies/data/models/movie_model.dart';
 import 'package:coolmovies/features/coolmovies/presentation/bloc/movies_bloc.dart';
+import 'package:coolmovies/features/coolmovies/presentation/widgets/movie_card_shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:searchable_listview/searchable_listview.dart';
 
 class MoviesHomePage extends StatelessWidget {
   const MoviesHomePage({super.key});
@@ -9,17 +12,108 @@ class MoviesHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Coolmovies'),
+        title: const Text('Coolmovies'),
       ),
       body: BlocBuilder<MoviesBloc, MoviesState>(
         builder: (context, state) {
           if (state is MoviesLoaded) {
-            return Center(
-              child: Text(state.movies.first.title),
+            return Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SearchableList<Movie>(
+                displaySortWidget: true,
+                sortPredicate: (a, b) =>
+                    (a.releaseDate ?? '').compareTo(b.releaseDate ?? ''),
+                // sortPredicate: (a, b) => a.title.compareTo(b.title),
+                builder: (list, index, movie) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: MovieTile(movie: movie),
+                  );
+                },
+                initialList: state.movies,
+                filter: (p0) {
+                  return state.movies
+                      .where((element) => element.title.contains(p0))
+                      .toList();
+                },
+                inputDecoration: InputDecoration(
+                  labelText: "Search Movie",
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
             );
           }
           return Container();
         },
+      ),
+    );
+  }
+}
+
+class MovieTile extends StatelessWidget {
+  final Movie movie;
+  const MovieTile({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: SizedBox(
+        height: 150.0,
+        child: Row(
+          children: [
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Hero(
+                  tag: 'hero-image:${movie.id}',
+                  child: movie.imgUrl != null
+                      ? Image.network(
+                          movie.imgUrl!,
+                          fit: BoxFit.cover,
+                          loadingBuilder: ((context, child, loadingProgress) {
+                            if (loadingProgress == null) return child;
+                            return const ShimmerBox(
+                              width: 80.0,
+                              height: 80.0,
+                            );
+                          }),
+                        )
+                      : const Placeholder(),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10.0),
+            Flexible(
+              flex: 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    movie.title,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 18.0),
+                  ),
+                  const Text(
+                    'Release Date',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(movie.releaseDate ?? ''),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
